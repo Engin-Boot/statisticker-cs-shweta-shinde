052c12bed79e54cf712372ccbb310aeb1e5fec61
shweta-shinde <47037519+shweta-shinde@users.noreply.github.com>
2020-09-03 19:57:02 +0530

    Completed code and test cases

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Statistics
 {
-    public class StatsComputer
+    public class Stats
     {
-        public Stats CalculateStatistics(List<float> numbers) {
-            //Implement statistics here
+        public float average;
+        public float max;
+        public float min;
+        public Stats()
+        {
+            this.average = float.NaN;
+            this.max = float.NaN;
+            this.min = float.NaN;
         }
     }
+    public class StatsComputer
+    {  
+        public Stats CalculateStatistics(List<float> numbers) {
+            
+            //Implement statistics here
+            Stats stats = new Stats();
+            if (numbers.Count!= 0)
+            {
+                if (numbers.Contains(float.NaN))
+                {
+                    numbers.RemoveAll(float.IsNaN);
+                }
+                stats.average = numbers.Average();
+                stats.max = numbers.Max();
+                stats.min = numbers.Min();       
+            }
+            return stats;
+        }  
+    }  
 }
